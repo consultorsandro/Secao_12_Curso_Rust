5a50d9f7a6b2035e7d0b5c6937e036f61a66b0cd
Sandro Reis <consultorsandro@hotmail.com>
2025-05-29 09:25:07 -0300

    Class 199 match with Option

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
+use std::os::windows::io::InvalidHandleError;
+
 fn main() {
-      let musical_instrument = [
+    //Class 199
+    let musical_instrument = [
         String::from("Guitar"),
         String::from("Piano"),
         String::from("Bass"),
     ];
 
-    let bass: Option<&String> = musical_instrument.get(2); 
+    let bass: Option<&String> = musical_instrument.get(2);
+    play(bass);
+    println!("{:?}", bass);
+
+    let invalid_instrument = musical_instrument.get(100); // Class 198
+    play(invalid_instrument);
+}
+fn play(instrument_option: Option<&String>) {
+    match instrument_option {
+        Option::Some(instrument) => println!("Playing the {}!", instrument),
+        Option::None => println!("Singing with my voice!"),
+    }
+}
+
+/*
+let musical_instrument = [
+        String::from("Guitar"),
+        String::from("Piano"),
+        String::from("Bass"),
+    ];
+
+    let bass: Option<&String> = musical_instrument.get(2);
     println!("Bass: {:?}", bass);
     let valid_instrument = bass.unwrap(); // Class 198
     println!("{}", valid_instrument);
@@ -13,8 +37,7 @@ fn main() {
     let invalid_instrument: Option<&String> = musical_instrument.get(3);
     println!("Invalid Instrument: {:?}", invalid_instrument);
     invalid_instrument.expect("This instrument does not exist in the list!"); // Class 198
-
-}
+ */
 /*
 // Class 196
 
